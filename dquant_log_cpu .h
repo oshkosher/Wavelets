85de9f4c5511b253dd4ba0420ec72a572b07f091
Bonnie Smithson <Bonnie@DejaThoris.com>
2014-08-27 13:04:54 -0700

    Partial implementations for loq quand and dequant, and uniform dquant.
    Not ready for testing.  But I don't want to lose it.

--- dquant_log_cpu .h
+++ dquant_log_cpu .h
@@ -1,10 +1,9 @@
-#ifndef __QUANT_LOG_CPU__
-#define __QUANT_LOG_CPU__
+#ifndef __DQUANT_LOG_CPU__
+#define __DQUANT_LOG_CPU__
 
-// Applies the threshold such that values <= threshold are 0
-// Maps the remaining range of values to the values 0:(2^bits)-1
-// Overwrites data with the new values
-void dquant_log_cpu(int len, float *data, int bits, float threshold, float maxVal);
+// Dequantizes the compressed data and outputs the dquantized data
+// The dquantized data is allocated here and must later be deleted.
+float * dquant_log_cpu(int len, float *data, int bits, float threshold, float maxVal);
 
 
-#endif // __QUANT_LOG_CPU__
+#endif // __DQUANT_LOG_CPU__
--- quant_log_cpu.h
+++ quant_log_cpu.h
@@ -4,7 +4,7 @@
 // Applies the threshold such that values <= threshold are 0
 // Maps the remaining range of values to the values 0:(2^bits)-1
 // Overwrites data with the new values
-void quant_log_cpu(int len, float *data, int bits, float threshold, float maxVal);
+float  quant_log_cpu(int len, float *data, int bits, float threshold, float maxVal);
 
 
 #endif // __QUANT_LOG_CPU__
