cd1b1bd858b17918ba77e2679904aa7224082581
Bonnie Smithson <Bonnie@DejaThoris.com>
2014-09-17 15:49:50 -0700

    Display an error message if run against a text data file
    If that first string is not "binary" the file is the wrong type.

--- Matlab/readDataFile.m
+++ Matlab/readDataFile.m
@@ -1,10 +1,14 @@
-% XX = readDataFile("chicago_orig.data");
 function AA=readDataFile(filename)
 dataFl = fopen(filename,"r");
 [sss,ccc,err] = fscanf(dataFl,"%s\n","C");
+if ( strcmp(sss,"binary") == 0)
+	display("Wrong type!  You need a binary .data file")
+	AA = 0;
+else
 [dim,cnt] = fread(dataFl,2,"int32");
 [ddd,cnt]=fread(dataFl,Inf,"float32");
 data=reshape(ddd,dim);
 fclose(dataFl);
 figure;imagesc(data');colormap (gray(256));title(filename);axis equal;axis([1 dim(2) 1 dim(1)]);
 AA = data';
+end
