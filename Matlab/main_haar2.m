738f1f12cddc9010d4153a599de4358feac19309
szarantonello <szarantonelo@scu.edu>
2014-09-23 14:14:43 -0700

    Update main_haar2.m

--- Matlab/main_haar2.m
+++ Matlab/main_haar2.m
@@ -6,7 +6,7 @@ A=double(A);                         % convert A to floating points
 figure(1);image(A);colormap gray(256);%axis equal;axis([1 256 1 256]);
 
 % Apply transformation
-B = haar_encode(A);
+B = haar2_encode(A);
 %B = d4_encode(A);
 
 %figure(2);imagesc(B)
@@ -23,7 +23,7 @@ BU = codebook(indx+1);b=find(BU==0);mb=length(b);
 % Create 2D array
 BU=reshape(BU,dim1,dim2);
 % Inverse transformation
-AU = haar_decode(BU);
+AU = haar2_decode(BU);
 % AU = d4_decode(BU);
 figure(3);image(AU);colormap gray(256);title('Uniform Quant');%axis equal;axis([1 256 1 256]);
         
