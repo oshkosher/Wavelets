8534ee4db3d0f3ff9e9d122f3066bd9d51557148
Ed K <thickerpad@karrels.org>
2015-01-26 17:17:30 -1000

    add missing java build file

---
+++ makeall.java
@@ -0,0 +1,12 @@
+class makeall {
+  
+  static {
+    Class c = Cubelet.class;
+    c = CubeletFile.class;
+    c = CubeletViewer.class;
+    c = ImageDiff.class;
+    c = WaveletCompress.class;
+    c = WaveletSampleImage.class;
+  }
+
+}
