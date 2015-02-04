76347db83cc097e672fe09e02252661030da45f0
Degryll <x@x.com>
2015-02-04 20:52:37 +0100

    fix CUDA wavelet transform
    - Works for size until 256x256x256
    - I'm working to improve that.

--- CUDA/wavelet/wavelet.h
+++ CUDA/wavelet/wavelet.h
@@ -3,5 +3,8 @@
 
 extern "C" void setUpFilter(const float *filter);
 
+extern "C" void fwt_1D_GPU(float *data, const unsigned level, const unsigned nx, const unsigned ny);
+extern "C" void iwt_1D_GPU(float *data, const unsigned level, const unsigned nx, const unsigned ny);
+
 extern "C" void  comp(float *data, const unsigned nx, const unsigned ny, const unsigned nz, const unsigned lvlx, const unsigned lvly, const unsigned lvlz);
 extern "C" void ucomp(float *data, const unsigned nx, const unsigned ny, const unsigned nz, const unsigned lvlx, const unsigned lvly, const unsigned lvlz);
